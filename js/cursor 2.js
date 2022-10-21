55ad57f13dd65c18eff63037b9aa2264b864a7c8
muzi981 <2428412161@qq.com>
2022-10-21 11:08:48 +0800

    Site updated: 2022-10-21 11:08:48

---
+++ js/cursor 2.js
@@ -0,0 +1,237 @@
+/*!
+ * Fairy Dust Cursor.js
+ * - 90's cursors collection
+ * -- https://github.com/tholman/90s-cursor-effects
+ * -- http://codepen.io/tholman/full/jWmZxZ/
+ */
+
+//鼠标点击雪花特效
+(function fairyDustCursor() {
+
+    var possibleColors = ["#D61C59", "#E7D84B", "#1B8798"]
+    var width = window.innerWidth;
+    var height = window.innerHeight;
+    var cursor = { x: width / 2, y: width / 2 };
+    var particles = [];
+
+    function init() {
+        bindEvents();
+        loop();
+    }
+
+    // Bind events that are needed
+    function bindEvents() {
+        document.addEventListener('mousemove', onMouseMove);
+        document.addEventListener('touchmove', onTouchMove);
+        document.addEventListener('touchstart', onTouchMove);
+
+        window.addEventListener('resize', onWindowResize);
+    }
+
+    function onWindowResize(e) {
+        width = window.innerWidth;
+        height = window.innerHeight;
+    }
+
+    function onTouchMove(e) {
+        if (e.touches.length > 0) {
+            for (var i = 0; i < e.touches.length; i++) {
+                addParticle(e.touches[i].clientX, e.touches[i].clientY, possibleColors[Math.floor(Math.random() * possibleColors.length)]);
+            }
+        }
+    }
+
+    function onMouseMove(e) {
+        cursor.x = e.clientX;
+        cursor.y = e.clientY;
+
+        addParticle(cursor.x, cursor.y, possibleColors[Math.floor(Math.random() * possibleColors.length)]);
+    }
+
+    function addParticle(x, y, color) {
+        var particle = new Particle();
+        particle.init(x, y, color);
+        particles.push(particle);
+    }
+
+    function updateParticles() {
+
+        // Updated
+        for (var i = 0; i < particles.length; i++) {
+            particles[i].update();
+        }
+
+        // Remove dead particles
+        for (var i = particles.length - 1; i >= 0; i--) {
+            if (particles[i].lifeSpan < 0) {
+                particles[i].die();
+                particles.splice(i, 1);
+            }
+        }
+
+    }
+
+    function loop() {
+        requestAnimationFrame(loop);
+        updateParticles();
+    }
+
+    /**
+     * Particles
+     */
+
+    function Particle() {
+
+        this.character = "*";
+        this.lifeSpan = 120; //ms
+        this.initialStyles = {
+            "position": "fixed",
+            "top": "0", //必须加
+            "display": "block",
+            "pointerEvents": "none",
+            "z-index": "10000000",
+            "fontSize": "20px",
+            "will-change": "transform"
+        };
+
+        // Init, and set properties
+        this.init = function(x, y, color) {
+
+            this.velocity = {
+                x: (Math.random() < 0.5 ? -1 : 1) * (Math.random() / 2),
+                y: 1
+            };
+
+            this.position = { x: x - 10, y: y - 20 };
+            this.initialStyles.color = color;
+
+            this.element = document.createElement('span');
+            this.element.innerHTML = this.character;
+            applyProperties(this.element, this.initialStyles);
+            this.update();
+
+            document.body.appendChild(this.element);
+        };
+
+        this.update = function() {
+            this.position.x += this.velocity.x;
+            this.position.y += this.velocity.y;
+            this.lifeSpan--;
+
+            this.element.style.transform = "translate3d(" + this.position.x + "px," + this.position.y + "px,0) scale(" + (this.lifeSpan / 120) + ")";
+        }
+
+        this.die = function() {
+            this.element.parentNode.removeChild(this.element);
+        }
+
+    }
+
+    /**
+     * Utils
+     */
+
+    // Applies css `properties` to an element.
+    function applyProperties(target, properties) {
+        for (var key in properties) {
+            target.style[key] = properties[key];
+        }
+    }
+
+    init();
+})();
+
+// f12禁用
+function forbidden_control() {
+    $.extend({
+        message: function(a) {
+            var b = {
+                title: "",
+                message: "操作成功",
+                time: "3000",
+                type: "success",
+                showClose: !0,
+                autoClose: !0,
+                onClose: function() {}
+            };
+            "string" == typeof a && (b.message = a), "object" == typeof a && (b = $.extend({}, b, a));
+            var c, d, e, f = b.showClose ? '<div class="c-message--close">×</div>' : "",
+                g = "" !== b.title ? '<h2 class="c-message__title">' + b.title + "</h2>" : "",
+                h = '<div class="c-message animated animated-lento slideInRight"><i class=" c-message--icon c-message--' + b.type + '"></i><div class="el-notification__group">' + g + '<div class="el-notification__content">' + b.message + "</div>" + f + "</div></div>",
+                i = $("body"),
+                j = $(h);
+            d = function() {
+                j.addClass("slideOutRight"), j.one("webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend", function() {
+                    e()
+                })
+            }, e = function() {
+                j.remove(), b.onClose(b), clearTimeout(c)
+            }, $(".c-message").remove(), i.append(j), j.one("webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend", function() {
+                j.removeClass("messageFadeInDown")
+            }), i.on("click", ".c-message--close", function(a) {
+                d()
+            }), b.autoClose && (c = setTimeout(function() {
+                d()
+            }, b.time))
+        }
+    }),
+    document.onkeydown = function(e) {
+        if (123 == e.keyCode || e.ctrlKey && e.shiftKey && (74 === e.keyCode || 73 === e.keyCode || 67 === e.keyCode) || (e.ctrlKey && 85 === e.keyCode)) return $.message({
+            message: "采用本站js及css请注明来源，禁止商业使用！",
+            title: "你真坏，不能打开控制台喔!",
+            type: "error",
+            autoHide: !1,
+            time: "3000"
+        }), event.keyCode = 0, event.returnValue = !1, !1
+    }, document.oncontextmenu = function() {
+        return $.message({
+            message: "采用本站js及css请注明来源，禁止商业使用！",
+            title: "不能右键/长按喔！",
+            type: "error",
+            autoHide: !1,
+            time: "3000"
+        }), !1
+    },
+    function() {
+        function e() {
+            var e = new Date;
+            if (new Date - e > 10) {
+                try {
+                    document.getElementsByTagName("html")[0].innerHTML = '<div style="width: 100%;height: 50px;font-size: 30px;text-align: center;font-weight: bold;">啊这个网站炸了，<a href="/" style="color:#4285f4;">点击返回</a>试试吧~</div>'
+                } catch (e) {}
+                return document.body.innerHTML = '<div style="width: 100%;height: 50px;font-size: 30px;text-align: center;font-weight: bold;">啊这个网站炸了，<a href="/" style="color:#4285f4;">点击返回</a>试试吧~</div>', !0
+            }
+            return !1
+        }
+
+        function t() {
+            for (; e();) e()
+        }
+        e() ? t() : window.onblur = function() {
+            setTimeout(function() {
+                t()
+            }, 500)
+        }
+    }(),
+    function() {
+        var e = /x/;
+        e.toString = function() {
+            try {
+                document.getElementsByTagName("html")[0].innerHTML = '<div style="width: 100%;height: 50px;font-size: 30px;text-align: center;font-weight: bold;">啊这个网站炸了，<a href="/" style="color:#4285f4;">点击返回</a>试试吧~</div>'
+            } catch (e) {}
+            return document.body.innerHTML = '<div style="width: 100%;height: 50px;font-size: 30px;text-align: center;font-weight: bold;">啊这个网站炸了，<a href="/" style="color:#4285f4;">点击返回</a>试试吧~</div>', "禁止打开控制台！"
+        }
+    }(),
+    function() {
+        var e = document.createElement("div");
+        Object.defineProperty(e, "id", {
+            get: function() {
+                try {
+                    document.getElementsByTagName("html")[0].innerHTML = '<div style="width: 100%;height: 50px;font-size: 30px;text-align: center;font-weight: bold;">啊这个网站炸了，<a href="/" style="color:#4285f4;">点击返回</a>试试吧~</div>'
+                } catch (e) {}
+                document.body.innerHTML = '<div style="width: 100%;height: 50px;font-size: 30px;text-align: center;font-weight: bold;">啊这个网站炸了，<a href="/" style="color:#4285f4;">点击返回</a>试试吧~</div>'
+            }
+        }), console.log(e)
+    }()
+}
+forbidden_control();
